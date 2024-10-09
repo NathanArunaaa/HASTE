0a38863dbf3a0b9897929d730e2a860a6ebed788
NathanArunaaa <nathanaruna07@gmail.com>
2024-10-09 15:49:01 -0400

    ser change

--- control-panel/main.py
+++ control-panel/main.py
@@ -43,7 +43,7 @@ class App(customtkinter.CTk):
         self.sample_loaded = False
 
         #------inits------- 
-        self.ser = serial.Serial('/dev/serial0', 9600, timeout=1)
+        self.ser = serial.Serial()
         self.ser.flush()
         self.title("HASTE CONTROL PANEL")
         self.config(cursor="none")
