6698eee9c6015b869e40784487a130fe9167f708
NathanArunaaa <nathanaruna07@gmail.com>
2024-12-03 14:15:22 -0500

    test

--- system-controller/functions.py
+++ system-controller/functions.py
@@ -71,7 +71,7 @@ def cut_sections(num_sections):
 
     try:
         step_motor(Y_DIR_PIN, Y_STEP_PIN, CCW, 4000)
-        step_motor(X_DIR_PIN, X_STEP_PIN, CCW, 10000)
+        step_motor(X_DIR_PIN, X_STEP_PIN, CCW, 11000)
 
         for section in range(num_sections):
             print(f"Cutting section {section + 1}...")
