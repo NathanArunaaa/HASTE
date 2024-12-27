f5eb3bc2b34a41f7fdbcd7784f9ea3798011776c
NathanArunaaa <nathanaruna07@gmail.com>
2024-12-26 19:02:40 -0500

    update video label alignment and remove redundant temperature update thread

--- control-panel/main.py
+++ control-panel/main.py
@@ -111,8 +111,8 @@ class App(customtkinter.CTk):
         
         self.video_frame = customtkinter.CTkFrame(self.video_feeds_frame, fg_color="white")
         self.video_frame.grid(row=1, column=0, padx=(20, 0), pady=(20, 0), sticky="nsew")
-        self.video_label = customtkinter.CTkLabel(self.video_frame, text="")
-        self.video_label.grid(row=0, column=0, padx=20, pady=20) 
+        self.video_label = customtkinter.CTkLabel(self.video_frame, text="", anchor="center")
+        self.video_label.grid(row=0, column=0, padx=20, pady=20)
         
         threading.Thread(target=self.update_video_feed, daemon=True).start()
         
@@ -176,7 +176,6 @@ class App(customtkinter.CTk):
         self.water_temp = customtkinter.CTkLabel(self.scrollable_frame, text="Water Temp: --°C")
         self.water_temp.grid(row=0, column=0, padx=20, pady=20, sticky="nsew")   
         
-        threading.Thread(target=self.update_temperature, daemon=True).start()
 
         #------Default values-------
         self.textbox.insert("0.0", "Developed By: Nathan Aruna & Arielle Benarroch\n\n" + "Console Log:\n\n" )
