a30edbb5c77b0777d4bbea8ad28f5684730e1d9b
NathanArunaaa <nathanaruna07@gmail.com>
2025-02-11 12:27:59 -0500

    Refactor video label dimensions and grid configuration in control panel UI

--- control-panel/main.py
+++ control-panel/main.py
@@ -115,14 +115,14 @@ class App(customtkinter.CTk):
         #------Video Feeds-------
         self.video_feeds_frame = customtkinter.CTkFrame(self, fg_color="white")
         self.video_feeds_frame.grid(row=1, column=1, padx=(20, 20), pady=(20, 20), sticky="nsew")
-        self.video_feeds_frame.grid_columnconfigure(0, weight=1)
-        self.video_feeds_frame.grid_rowconfigure(4, weight=1)
+        self.video_frame.grid_columnconfigure(0, weight=1)  # Allow expansion
+        self.video_frame.grid_rowconfigure(0, weight=1)   
 
         self.video_frame = customtkinter.CTkFrame(self.video_feeds_frame, fg_color="gray")  
         self.video_frame.grid(row=1, column=0, padx=(20, 20), pady=(20, 20), sticky="nsew")
 
-        self.video_label = customtkinter.CTkLabel(self.video_frame, text="", anchor="center", width=600, height=200)  
-        self.video_label.grid(row=0, column=0, padx=20, pady=20)
+        self.video_label = customtkinter.CTkLabel(self.video_frame, text="", width=500, height=300)  
+        self.video_label.grid(row=0, column=0, sticky="nsew")
         
         self.video_label.grid_propagate(False)
 
