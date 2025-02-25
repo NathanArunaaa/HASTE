1b55f3e60c399f4321b08853a52805c7c8294dd0
NathanArunaaa <nathanaruna07@gmail.com>
2025-02-25 15:19:02 -0500

    Fix face_sample function to use X2_LIMIT_PIN for limit switch check

--- system-controller/functions.py
+++ system-controller/functions.py
@@ -168,8 +168,9 @@ def home_motor():
 def face_sample(num_sections):
     try:
         step_motor(Y_DIR_PIN, Y_STEP_PIN, CCW, 4000)
+        GPIO.output(X_DIR_PIN, CW) 
 
-        while GPIO.input(22) == GPIO.LOW:  
+        while GPIO.input(X2_LIMIT_PIN) == GPIO.LOW:  
             step_motor(X_DIR_PIN, X_STEP_PIN, CCW, 10 )
 
         for section in range(num_sections):
